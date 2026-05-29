58fa3b055a21ef231755d4a08c5fb8e23600c599
Gonzalo <gonz.moreira@gmail.com>
2026-05-29 10:44:22 +0200

    Botones de Receta favorita y hecha

--- js/main.js
+++ js/main.js
@@ -1,46 +1,8 @@
-import { recetas } from "./receta.js";
-import * as funciones from './receta.js'
+import { recetas, totalRecetas, crearTarjetaReceta, mostrarRecetas } from "./receta.js";
 
-const listaTotalRecetas = document.querySelector("#recetas")
-const totalRecetas = document.querySelector("#totalRecetas");
+const totalRecetasHTML = document.querySelector("#totalRecetas");
 
-//Muestra el numero total de recetas en el HTML
-totalRecetas.textContent = funciones.totalRecetas(recetas)
-
-//Muestra cada receta y sus datos
-function mostrarRecetas(listaRecetas) {
-
-    // Limpia el contenido del contenedor antes de mostrar las recetas
-    listaTotalRecetas.innerHTML = "";
-
-    listaRecetas.forEach(receta => {
-        const div = document.createElement("div");
-
-        div.classList.add("card-receta");
-
-        div.innerHTML = `
-            <h3 class="titulo-receta">
-                ${receta.nombre}
-                <span class="categoria ${receta.categoria.toLowerCase()}">
-                    ${receta.categoria}
-                </span>
-            </h3>
-
-            <p><strong>Tiempo:</strong> ${receta.tiempoPreparacion} min</p>
-            <p><strong>Favorita:</strong> ${receta.favorita ? "⭐ Sí" : "❌ No"}</p>
-            <p><strong>Hecha:</strong> ${receta.receta_hecha ? "✔ Sí" : "❌ No"}</p>
-
-            <div class="ingredientes">
-                <strong>Ingredientes:</strong><br>
-                ${receta.ingredientes
-                .map(i => i.charAt(0).toUpperCase() + i.slice(1))
-                .join(", ")}
-            </div>
-        `;
-
-        listaTotalRecetas.appendChild(div);
-    });
-
-}
+// Mostrar total
+totalRecetasHTML.textContent = totalRecetas(recetas);
 
 mostrarRecetas(recetas);
--- js/receta.js
+++ js/receta.js
@@ -1,8 +1,8 @@
 export const recetas = [
     {
-        id:1,
+        id: 1,
         nombre: "Pizza Margarita",
-        categoria: "Plato principal",
+        categoria: "Principal",
         receta_hecha: false,
         favorita: false,
         tiempoPreparacion: 30,
@@ -14,7 +14,7 @@ export const recetas = [
         ]
     },
     {
-        id:2,
+        id: 2,
         nombre: "Patatas bravas",
         categoria: "Entrante",
         receta_hecha: false,
@@ -29,9 +29,9 @@ export const recetas = [
         ]
     },
     {
-        id:3,
+        id: 3,
         nombre: "Tortilla de patatas",
-        categoria: "Plato principal",
+        categoria: "Principal",
         receta_hecha: true,
         favorita: true,
         tiempoPreparacion: 40,
@@ -44,7 +44,7 @@ export const recetas = [
         ]
     },
     {
-        id:4,
+        id: 4,
         nombre: "Brownie de chocolate",
         categoria: "Postre",
         receta_hecha: false,
@@ -61,6 +61,82 @@ export const recetas = [
     }
 ];
 
+//Crea las tarjetas en el HTML
+export function crearTarjetaReceta(receta) {
+
+    const div = document.createElement("div");
+    div.classList.add("card-receta", receta.categoria.toLowerCase());
+
+    div.innerHTML = `
+        <h3 class="titulo-receta">
+            ${receta.nombre}
+            <span class="categoria ${receta.categoria.toLowerCase()}">
+                ${receta.categoria}
+            </span>
+        </h3>
+
+        <p><strong>Tiempo:</strong> ${receta.tiempoPreparacion} min</p>
+        <p><strong>Favorita:</strong> ${receta.favorita ? "⭐ Sí" : "❌ No"}</p>
+        <p><strong>Hecha:</strong> ${receta.receta_hecha ? "✔ Sí" : "❌ No"}</p>
+
+        <div class="ingredientes">
+            <strong>Ingredientes:</strong><br>
+            ${receta.ingredientes
+            .map(i => i.charAt(0).toUpperCase() + i.slice(1))
+            .join(", ")}
+        </div>
+
+        <button 
+            class="${receta.favorita ? "favorita" : "no-favorita"}"
+            data-id="${receta.id}">
+                ${receta.favorita
+                ? "Quitar de favorita"
+                : "Añadir a favorita"}
+        </button>
+
+        <button 
+            class="${receta.receta_hecha ? "hecha" : "no-hecha"}"
+            data-id="${receta.id}">
+                ${receta.receta_hecha
+                ? "Receta hecha"
+                : "Receta sin hacer"}
+        </button>
+
+    `;
+
+    const botones = div.querySelectorAll("button");
+
+    //Seleccionamos el primer boton (receta favorita)
+    const botonFavorita = botones[0];
+
+    //Botón de reeta hecha
+    const botonHecha = botones[1];
+
+    //La acción que hace cuando haces click al botón
+    botonFavorita.addEventListener("click", () => cambiarEstadoFavorita(receta))
+
+    botonHecha.addEventListener("click", () => cambiarEstadoHecha(receta))
+
+    return div;
+}
+
+// Mostrar todas las recetas
+export function mostrarRecetas(listaRecetas) {
+
+    const listaTotalRecetas =
+        document.querySelector("#recetas");
+
+    listaTotalRecetas.innerHTML = "";
+
+    listaRecetas.forEach(receta => {
+
+        listaTotalRecetas.appendChild(
+            crearTarjetaReceta(receta)
+        );
+
+    });
+}
+
 //Contar el total de recetas
 export function totalRecetas(recetas) {
     return recetas.length;
@@ -69,4 +145,16 @@ export function totalRecetas(recetas) {
 //CRUD
 //Filtrar por categorías
 //Marcar como hecha
+function cambiarEstadoHecha(receta) {
+    receta.receta_hecha = !receta.receta_hecha
+
+    //Para cargar los cambios
+    mostrarRecetas(recetas)
+}
 //Marcar como favorita
+function cambiarEstadoFavorita(receta) {
+    receta.favorita = !receta.favorita
+
+    //Para cargar los cambios
+    mostrarRecetas(recetas)
+}
